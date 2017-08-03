e407f565c866d796eb01f501e0a1fb5eef40302d
sani07mobi <pramod.v@mobisofttech.co.in>
2017-08-03 19:17:08 +0530

    Update User Profile

--- app/components/profile/profileController.js
+++ app/components/profile/profileController.js
@@ -3,13 +3,15 @@ altairApp
         '$scope',
         '$rootScope',
          'apiGetData',
+         'apiPostData',
          '$cookieStore',
-        function($scope, $rootScope,apiGetData,$cookieStore){
+        function($scope, $rootScope,apiGetData,apiPostData,$cookieStore){
         	$rootScope.globals = $cookieStore.get('globals') || {};
             $rootScope.u_id = $rootScope.globals.currentUser.u_id;
 
 
         	getData();
+            getUserBalance();
             $scope.profile = {};
             $scope.userData;
              function getData(){
@@ -22,11 +24,56 @@ altairApp
                     $scope.data = $scope.responseData.data;
                     if($scope.data.code == 302){
                         $scope.userData = $scope.data.data;
-                       $scope.userProduct = $scope.userData[0]['userProduct'];
+                        
                         console.log($scope.userData[0]['userProduct']);
                         //console.log($scope.userProduct);
                     }
                 });
              }
+            function getUserBalance()
+             {
+               
+                var getUserBalanceDetails = "getBalanceByUserId/"+$rootScope.u_id;                
+
+                apiGetData.async(getUserBalanceDetails).then(function(d) {
+                    $scope.responseData = d;
+
+                    $scope.data = $scope.responseData.data;
+
+                    if($scope.data.code == 302){
+                      
+                        $scope.userBalanceData = $scope.data.data;  
+                        //$scope.userProduct = $scope.userBalanceData[0]['userProduct'];                     
+                        console.log($scope.userBalanceData);
+                        //console.log($scope.userProduct);
+                    }
+                });
+                
+             }
+             $scope.updateUserDetails = function(userdata){
+                alert("kldjlasd");
+                var userDetailsData = JSON.stringify(userdata);               
+                var updateUserDetails = "updateUserById/"+$rootScope.u_id;
+
+                apiPostData.async(updateUserDetails, userDetailsData).then(function(d) {
+                    $scope.data = d.data;
+                    if($scope.data.code == 200){
+                        var modal = UIkit.modal.blockUI('<div class=\'uk-text-center\'>Data Updated Successfully');
+                            modal.show();
+                           // getData();
+                            setTimeout(function(){
+                                modal.hide();
+                                getData();
+                               
+                        },3000);
+                        
+                    }
+                    else if($scope.data.code == 400)
+                    {
+                        UIkit.modal.alert('Somethig Going Worng........');
+                    }
+                });
+             };
+            
         }
     ]);
