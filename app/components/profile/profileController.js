46f758ca97439d8e31e172181f759e5576cb481e
tusharkharat007 <tushar.k@mobisofttech.co.in>
2017-09-14 19:03:11 +0530

    update user profile

--- app/components/profile/profileController.js
+++ app/components/profile/profileController.js
@@ -1,79 +1,131 @@
 angular
-    .module('altairApp')
-    .controller('profileCtrl', [
-        '$scope',
-        '$rootScope',
-        'apiGetData',
-        'apiPostData',
-        '$cookieStore',
-        function($scope, $rootScope,apiGetData,apiPostData,$cookieStore){
-        	
-            $scope.profile = {};
-            $scope.userData ="";
+.module('altairApp')
+.controller('profileCtrl', [
+    '$scope',
+    '$rootScope',
+    'apiGetData',
+    'apiPostData',
+    '$cookieStore',
+    function($scope, $rootScope,apiGetData,apiPostData,$cookieStore){
 
-        	getData();
-            getUserBalance();
-            
-             function getData(){
-                var getUserDetails = "getUserById/"+$rootScope.u_id;                
+        $scope.profile = {};
+        $scope.userData ="";
+        $scope.passwordFlag = true;        
 
-                apiGetData.async(getUserDetails).then(function(d) {
-                    $scope.responseData = d;
+        getData();
+        getUserBalance();
+
+        function getData(){
+            var getUserDetails = "getUserById/"+$rootScope.u_id;                
+
+            apiGetData.async(getUserDetails).then(function(d) {
+                $scope.responseData = d;
                     /*alert("hello data");
                     console.log('d data',d);*/
                     $scope.data = $scope.responseData.data;
                     if($scope.data.code == 302){
-                        $scope.userData = $scope.data.data;
+                        $scope.userData = $scope.data.data[0].userId;
                         
-                        console.log($scope.userData[0]['userProduct']);
-                        //console.log($scope.userProduct);
+                        console.log("getUserById: ",$scope.userData);
+                        console.log($scope.userData.name);
                     }
                 });
-             }
-            function getUserBalance()
-             {
-               
-                var getUserBalanceDetails = "getBalanceByUserId/"+$rootScope.u_id;                
+        }
+        function getUserBalance()
+        {
 
-                apiGetData.async(getUserBalanceDetails).then(function(d) {
-                    $scope.responseData = d;
+            var getUserBalanceDetails = "getBalanceByUserId/"+$rootScope.u_id;                
 
-                    $scope.data = $scope.responseData.data;
+            apiGetData.async(getUserBalanceDetails).then(function(d) {
+                $scope.responseData = d;
 
-                    if($scope.data.code == 302){
-                      
-                        $scope.userBalanceData = $scope.data.data;  
+                $scope.data = $scope.responseData.data;
+
+                if($scope.data.code == 302){
+
+                    $scope.userBalanceData = $scope.data.data;  
                         //$scope.userProduct = $scope.userBalanceData[0]['userProduct'];                     
                         console.log($scope.userBalanceData);
                         //console.log($scope.userProduct);
                     }
                 });
-                
-             }
-             $scope.updateUserDetails = function(userdata){
-                alert("kldjlasd");
-                var userDetailsData = JSON.stringify(userdata);               
-                var updateUserDetails = "updateUserById/"+$rootScope.u_id;
+
+        }
+        $scope.updateUserDetails = function(userdata){
+            
+            var userDetailsData = JSON.stringify(userdata);               
+            var updateUserDetails = "updateUserById/"+$rootScope.u_id;
+
+            //alert($scope.profileBtnFlag);
+
+            if($scope.profileBtnFlag == 0){
 
                 apiPostData.async(updateUserDetails, userDetailsData).then(function(d) {
                     $scope.data = d.data;
                     if($scope.data.code == 200){
                         var modal = UIkit.modal.alert('<div class=\'uk-text-center\'>Data Updated Successfully');
-                            modal.show();
+                        modal.show();
                            // getData();
-                            setTimeout(function(){
-                                modal.hide();
-                                getData();
-                               
+                        setTimeout(function(){
+                            modal.hide();
+                            getData();
                         },3000);
-                        
                     }
                     else if($scope.data.code == 400)
                     {
                         UIkit.modal.alert('Somethig Going Worng........');
                     }
                 });
-             };
+
+            }else if($scope.profileBtnFlag == 1){
+
+                if($scope.passwordFlag == false){
+                    return false;
+                }
+                alert("change pwd");
+                console.log("On Change Pwd: ",userdata);
+            }
+
             
+        };
+
+        $scope.profileBtnFlag = 0;
+
+        $scope.setProfileBtnFlag = function(flag){
+            $scope.profileBtnFlag = flag;
         }
-    ]);
+
+    }
+    ])
+    .directive('passwordVerify', passwordVerify);
+    
+
+function passwordVerify(){
+    return{
+        restrict: 'A',
+        require: '?ngModel',
+        link: function(scope, elem, attrs, ngModel){
+
+            if(!ngModel) return;
+
+            scope.$watch(attrs.ngModel, function(){
+                validate();
+            });
+
+            attrs.$observe('passwordVerify', function(val){
+                validate();
+            });
+
+            var validate = function(){
+
+                var val1 = ngModel.$viewValue;
+                var val2 = attrs.passwordVerify;
+
+                if(val1 != undefined){
+                    scope.passwordFlag = val1 === val2;
+                }                
+            };
+        }
+    }
+}
+
