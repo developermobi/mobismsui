c2d4f08591765344c4ec571900d8b679df231d63
tusharkharat007 <tushar.k@mobisofttech.co.in>
2017-11-07 17:17:08 +0530

    bug fix

--- app/shared/header/headerController.js
+++ app/shared/header/headerController.js
@@ -16,35 +16,42 @@ angular
                 return false;  
             }
 
-            var balance_url = 'getBalanceByUserId/'+$rootScope.u_id;
+            
 
-            apiGetData.async(balance_url).then(function(d) {
-                $scope.responseBalanceData = d.data;
-                //console.log('responseBalanceData: ',$scope.responseBalanceData);
+            $scope.getBalance = function(){
+                var balance_url = 'getBalanceByUserId/'+$rootScope.u_id;
 
-                if($scope.responseBalanceData.code == 302){
-                    
-                    //console.log('responseBalanceData length',$scope.responseBalanceData.data.length);
-                    $scope.balanceData = $scope.responseBalanceData.data;
-                    $.each($scope.balanceData,function(i){
+                apiGetData.async(balance_url).then(function(d) {
+                    $scope.responseBalanceData = d.data;
+                    console.log('responseBalanceData: ',$scope.responseBalanceData);
+
+                    if($scope.responseBalanceData.code == 302){
                         
-                        //console.log('balanceData[i]: ',$scope.balanceData[i]);
-                        if($scope.balanceData[i].productId.id == 1){
-                            $rootScope.userBalance.transactional = $scope.balanceData[i].balance;
-                        }else if($scope.balanceData[i].productId.id == 2){
-                            $rootScope.userBalance.promotional = $scope.balanceData[i].balance;
-                        }
+                        //console.log('responseBalanceData length',$scope.responseBalanceData.data.length);
+                        $scope.balanceData = $scope.responseBalanceData.data;
+                        $.each($scope.balanceData,function(i){
+                            
+                            //console.log('balanceData[i]: ',$scope.balanceData[i]);
+                            if($scope.balanceData[i].productId.id == 1){
+                                $rootScope.userBalance.transactional = $scope.balanceData[i].balance;
+                            }else if($scope.balanceData[i].productId.id == 2){
+                                $rootScope.userBalance.promotional = $scope.balanceData[i].balance;
+                            }
 
-                    });
-                }
-                else{
-                    var modal = UIkit.modal.alert('<div class=\'uk-text-center\'>'+$scope.responseBalanceData.message);
-                    modal.show();
-                }
+                        });
+                    }
+                    else{
+                        var modal = UIkit.modal.alert('<div class=\'uk-text-center\'>'+$scope.responseBalanceData.message);
+                        modal.show();
+                    }
 
-                // console.log('transactional bal: ',$rootScope.userBalance.transactional);
-                // console.log('promotional bal: ',$rootScope.userBalance.transactional);
-            });
+                    // console.log('transactional bal: ',$rootScope.userBalance.transactional);
+                    // console.log('promotional bal: ',$rootScope.userBalance.transactional);
+                });
+            }
+
+            $scope.getBalance();
+            
 
             $scope.submitSignOut = function(){                
                 AuthenticationService.ClearCredentials();
