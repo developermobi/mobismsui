c157390b3c9cf59b98177bf7626c1dc8aeb9b14e
sani07mobi <pramod.v@mobisofttech.co.in>
2017-11-23 16:16:14 +0530

    no chnage

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ var altairApp = angular.module('altairApp', [
 
 
 
- /*altairApp.value('globalUrl', 'http://localhost:8080/sms/api/');
+/* altairApp.value('globalUrl', 'http://localhost:8080/sms/api/');
  altairApp.value('appUrl', 'http://localhost:81/mobismsui/#/');
  altairApp.value('downloadUrl', 'http://localhost:8080/sms/download/');*/
 
